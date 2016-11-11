e1841c4d79a5702fda0ce45398a70296df99531d
Nate <nwoods@hep.wisc.edu>
2016-11-11 14:26:23 +0100

    Don't delete temporary file if this was already done (still need to figure out why this was ever an issue...).

--- SampleTools/Sample.py
+++ SampleTools/Sample.py
@@ -28,6 +28,7 @@ from glob import glob
 from math import sqrt
 from os import remove as _rm
 from os import close as _close
+from os.path import isfile as _isfile
 
 # Workaround for weird ROOT bug
 _dummy = Hist(1,0,1)
@@ -37,7 +38,7 @@ _dummy.draw()
 
 
 class _SampleBase(object):
-    def __init__(self, name, channel, dataIn, 
+    def __init__(self, name, channel, dataIn,
                  initFromMetadata=False, *args, **kwargs):
         self.name = name
         self.channel = channel
@@ -121,14 +122,14 @@ class _SampleBase(object):
 
 
 class NtupleSample(_SampleBase):
-    def __init__(self, name, channel, dataIn, initFromMetadata=False, 
+    def __init__(self, name, channel, dataIn, initFromMetadata=False,
                  *args, **kwargs):
         self.weight = ''
 
         super(NtupleSample, self).__init__(name, channel, dataIn, initFromMetadata, *args, **kwargs)
 
         self.oldNtuples = []
-            
+
     def storeInputs(self, inputs):
         if isinstance(inputs, Tree):
             self.ntuple = inputs
@@ -152,7 +153,7 @@ class NtupleSample(_SampleBase):
 
     def combineNtuples(self, files, chan):
         '''
-        Makes a TreeChain of the files (or a bare TTree if there's just one 
+        Makes a TreeChain of the files (or a bare TTree if there's just one
         file). No redundant row culling or anything.
         '''
         if len(files) == 1:
@@ -166,17 +167,17 @@ class NtupleSample(_SampleBase):
         self.ntuple.Draw(var, selection, 'goff', hist)
         hist.sumw2()
 
-    def makeHist(self, var, selection, binning, weight='', perUnitWidth=True, 
+    def makeHist(self, var, selection, binning, weight='', perUnitWidth=True,
                  postprocess=False, **kwargs):
         '''
-        If var, selection, and/or weight are iterables (which must be of the 
+        If var, selection, and/or weight are iterables (which must be of the
         same length), the hists from the resulting var/selection/weight sets
         will be added together. If one or two are strings, they are reused the
         appropriate number of times.
         '''
         if len(binning) != 3:
             binning = [binning]
-        
+
         # use TH1D instead of TH1F because some datasets are now big enough for
         # floating point stuff to matter (!!)
         h = Hist(*binning, type='D', title=self.prettyName, **self._format)
@@ -217,10 +218,10 @@ class NtupleSample(_SampleBase):
         return h
 
 
-    def makeHist2(self, varX, varY, selection, binningX, binningY, weight='', 
+    def makeHist2(self, varX, varY, selection, binningX, binningY, weight='',
                   postprocess=False, **kwargs):
         '''
-        If var[XY], selection, and/or weight are iterables (which must be of the 
+        If var[XY], selection, and/or weight are iterables (which must be of the
         same length), the hists from the resulting var/selection/weight sets
         will be added together. If some are strings, they are reused the
         appropriate number of times.
@@ -270,7 +271,7 @@ class NtupleSample(_SampleBase):
 
     def implicitWeight(self):
         return ''
-        
+
 
     def applyWeight(self, w, reset=False):
         '''
@@ -286,7 +287,7 @@ class NtupleSample(_SampleBase):
     @property
     def weight(self):
         '''
-        Get the weight not including the automatically set weights (cross 
+        Get the weight not including the automatically set weights (cross
         section etc.). I.e., only the weights set by the user.
         '''
         return self._weight
@@ -297,7 +298,7 @@ class NtupleSample(_SampleBase):
         self._weight = w
     def fullWeight(self):
         '''
-        Get the weight including the automatically set weights (cross 
+        Get the weight including the automatically set weights (cross
         section etc.).
         '''
         return combineWeights(self.weight, self.implicitWeight())
@@ -305,10 +306,10 @@ class NtupleSample(_SampleBase):
     def applyCut(self, cut='', name=''):
         '''
         cut (str, Cut, function, or other callable): selection for rows to copy.
-            If a string or Cut, the Tree will be copied with CopyTree using 
+            If a string or Cut, the Tree will be copied with CopyTree using
             that cut. If a function or other callable, each row will be copied
             if cut(row) evaluates to True.
-        name (str): name for new tree (defaults to name of old tree. Only used 
+        name (str): name for new tree (defaults to name of old tree. Only used
             if cut is callable).
         '''
         self.oldNtuples.append(self.ntuple)
@@ -316,7 +317,7 @@ class NtupleSample(_SampleBase):
             if not name:
                 name = self.oldNtuples[-1].GetName()
             self.ntuple = Tree(name)
-            self.ntuple.set_buffer(self.oldNtuples[-1]._buffer, 
+            self.ntuple.set_buffer(self.oldNtuples[-1]._buffer,
                                    create_branches=True)
             for row in self.oldNtuples[-1]:
                 if cut(row):
@@ -377,13 +378,13 @@ class MCSample(NtupleSample):
 
     def implicitWeight(self):
         if self.xsec > 0:
-            w = 'genWeight * {} * {} * {} / {}'.format(self.intLumi, self.xsec, 
+            w = 'genWeight * {} * {} * {} / {}'.format(self.intLumi, self.xsec,
                                                        self.kFactor, self.sumW)
         else:
             w = ''
 
         return combineWeights(super(MCSample, self).implicitWeight(), w)
-            
+
 
     def formatDefault(self):
         self.format(True, drawstyle='hist', fillstyle='solid', legendstyle='F')
@@ -433,7 +434,7 @@ class _FileWrapper(object):
     '''
     def __init__(self, f):
         self.f = f
-        
+
         # *** FIXME *** there must be a better way to do this
         self.descriptor = getattr(self.f, '_{}__fd'.format(self.f.__class__.__name__))
         self.name = getattr(self.f, '_{}__tmp_path'.format(self.f.__class__.__name__))
@@ -445,7 +446,8 @@ class _FileWrapper(object):
         except OSError:
             pass
         try:
-            _rm(self.name)
+            if _isfile(self.name):
+                _rm(self.name)
         except OSError:
             rlog.warning("Failed to delete temporary file {} -- you may "
                          "need to delete it manually".format(self.name))
@@ -457,7 +459,7 @@ class DataSample(NtupleSample):
 
     def combineNtuples(self, files, chan):
         '''
-        Combines a number of ntuples into a new ntuple in a temporary file, 
+        Combines a number of ntuples into a new ntuple in a temporary file,
         ensuring that each event only appears once. For now, the version
         of the event chosen is just the first one seen.
         '''
@@ -465,12 +467,12 @@ class DataSample(NtupleSample):
         if not hasattr(self, 'tempFile'):
             self.tempFile = _FileWrapper(TemporaryFile())
         self.tempFile.f.cd()
-        
+
         out = Tree('{}_{}_ntuple'.format(self.name, chan))
         out.set_buffer(chain._buffer, create_branches=True)
 
         found = set()
-        
+
         for i,ev in enumerate(chain):
             evID = (ev.run, ev.lumi, ev.evt)
             if evID in found:
@@ -478,7 +480,7 @@ class DataSample(NtupleSample):
 
             out.fill()
             found.add(evID)
-    
+
         return out
 
 
@@ -486,11 +488,11 @@ class DataSample(NtupleSample):
         self.format(True, drawstyle='PE', legendstyle='LPE')
 
 
-    def makeHist(self, var, selection, binning, weight='', perUnitWidth=True, 
+    def makeHist(self, var, selection, binning, weight='', perUnitWidth=True,
                  poissonErrors=False, postprocess=False, **kwargs):
         h = super(DataSample, self).makeHist(var, selection, binning, weight,
                                              perUnitWidth, **kwargs)
-        
+
         if poissonErrors:
             pois = h.poisson_errors()
             pois.SetTitle(self.prettyName)
