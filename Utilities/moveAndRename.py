ccc67bf7ed4e6595e2ea708614a05c58906d497a
Nate <nwoods@hep.wisc.edu>
2016-11-28 18:55:23 +0100

    os.makedirs just overwrites existing stuff rather than throwing an exception in Python 2.7 :(

--- Utilities/moveAndRename.py
+++ Utilities/moveAndRename.py
@@ -13,31 +13,33 @@ from shutil import move as _mv
 from shutil import copy as _cp
 from os import makedirs as _mkdir
 from os.path import join as _join
+from os.path import isdir as _isdir
+from os.path import exists as _exists
 
 
 def moveAndRename(outdir, extension='.root', copy=False, **samples):
-    try:
+    if not _exists(outdir):
         _mkdir(outdir)
-    except OSError:
-        pass
-    
+    elif not _isdir(outdir):
+        raise IOError("There is already some non-directory object called {}.".format(outdir))
+
     if copy:
         move = _cp
     else:
         move = _mv
-    
+
     for name, filePath in samples.iteritems():
         files = _glob(filePath)
-    
+
         for i, f in enumerate(files):
             newName = _join(outdir, '{}_{}{}'.format(name, i, extension))
             move(f, newName)
-    
+
 
 if __name__ == '__main__':
     parser = _Args(description=("Move groups of files, renaming them in a "
                                 "consistent way."))
-    
+
     parser.add_argument('outdir', type=str, nargs=1,
                         help=('Directory to place files (will be created '
                               'if needed).'))
@@ -52,13 +54,13 @@ if __name__ == '__main__':
                               'matching GLOB will be moved and renamed '
                               'NAME_n.root, where n is a counter so that each file '
                               'has a unique name.'))
-    
+
     args = parser.parse_args()
-    
-    
+
+
     if not args.g:
         raise IOError("No files specified with -g option")
-    
+
     groups = dict(args.g)
 
     moveAndRename(args.outdir[0], args.extension, args.cp, **groups)
