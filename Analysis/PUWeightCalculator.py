e1343c9a18188c59faa92610feb1e5459b433b6a
Nate <nwoods@hep.wisc.edu>
2017-01-25 10:07:42 +0100

    Get number of PU bins from MC distribution file.

--- Analysis/PUWeightCalculator.py
+++ Analysis/PUWeightCalculator.py
@@ -6,7 +6,7 @@ pileup distribution and makes a file with a histogram of the ratio.
 Data distributions can be made in a CMS environment with a command like
 $ pileupCalc.py -i dataLumi_DoubleMuon_23aug2016.json --inputLumiJSON /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt --calcMode true --minBiasXsec 69200 --maxPileupBin 50 --numPileupBins 50 ~/ZZTools/data/pileup/puDistData_69200_23aug2016.root
 
-The json file passed as the first argument above can be created from farmout 
+The json file passed as the first argument above can be created from farmout
 output with a command like
 $ /data/cms/farmout/jobReportSummary.py --json-out lumisIUsed.json `find /path/to/submit/directories/*.xml`
 
@@ -26,30 +26,33 @@ from rootpy import asrootpy
 from os import path, environ
 
 
-outputFileName = 'puWeight_69200_08sep2016.root'
-mcDistFileName = 'mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU.txt'
-dataDistFileTemplate = 'puDistData_{}_08sep2016.root'
+outputFileName = 'puWeight_69200_16jan2017.root'
+mcDistFileName = 'mix_2016_25ns_Moriond17MC_PoissonOOTPU.txt'
+dataDistFileTemplate = 'puDistData_{}_16jan2017.root'
 dataDistFileName = dataDistFileTemplate.format(69200)
 dataDistFileNameDn = dataDistFileTemplate.format(66017)
 dataDistFileNameUp = dataDistFileTemplate.format(72383)
-nbins = 50
 
 # make a histogram from the MC distribution text file
-puMC = Hist(50,0.,50.)
+puMCVals = []
 with open(path.join(environ['zzt'], 'data', 'pileup', mcDistFileName)) as fMC:
-    for i, n in enumerate(fMC):
-        puMC[i+1].value = float(n)
+    for n in fMC:
+        puMCVals.append(float(n))
+
+puMC = Hist(len(puMCVals),0.,float(len(puMCVals)))
+for i, n in enumerate(puMCVals):
+    puMC[i+1].value = n
 
 # Get the data distributions from root files
-with root_open(path.join(environ['zzt'], 'data', 
+with root_open(path.join(environ['zzt'], 'data',
                          'pileup', dataDistFileName)) as fData:
     puData = asrootpy(fData.pileup).clone()
     puData.SetDirectory(0)
-with root_open(path.join(environ['zzt'], 'data', 
+with root_open(path.join(environ['zzt'], 'data',
                          'pileup', dataDistFileNameDn)) as fDataDn:
     puDataDn = asrootpy(fDataDn.pileup).clone()
     puDataDn.SetDirectory(0)
-with root_open(path.join(environ['zzt'], 'data', 
+with root_open(path.join(environ['zzt'], 'data',
                          'pileup', dataDistFileNameUp)) as fDataUp:
     puDataUp = asrootpy(fDataUp.pileup).clone()
     puDataUp.SetDirectory(0)
@@ -72,5 +75,5 @@ with root_open(path.join(environ['zzt'], 'data', 'pileup', outputFileName),
     outUp = puDataUp.clone(name='puScaleFactor_up')
     outUp /= puMC
     outUp.write()
-                         
+
 
