7f185f8ba248aa54cb6828580aa478d354cf758e
Nate <nwoods@hep.wisc.edu>
2017-02-03 10:49:05 +0100

    New version of RooUnfold, keep CMSSW release around for environment.

--- setup.sh
+++ setup.sh
@@ -4,15 +4,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export zzt="${DIR}"
 
+if [ ! -d "$zzt"/recipe/cmssw ]; then
+    pushd "$zzt"/recipe
+    scram pro -n cmssw CMSSW CMSSW_8_0_26
+    popd
+fi
+pushd "$zzt"/recipe/cmssw/src
+cmsenv
+popd
+
 if [ -d "$zzt"/recipe/vpython ]; then
     echo "Activating python virtual environment"
     source "$zzt"/recipe/vpython/bin/activate
 fi
 
-export PYTHONPATH="${PYTHONPATH}":"${zzt}"
+export PYTHONPATH="${zzt}":"${PYTHONPATH}"
 
 echo "ZZTools setup complete"
 
 if [ -d "$zzt"/RooUnfold-1.1.1 ]; then
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$zzt"/RooUnfold-1.1.1
 fi
+if [ -d "$zzt"/RooUnfold ]; then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"$zzt"/RooUnfold
+fi
+
