93fdf40acebb17b94b48f1a1bb20e22adb5fd968
Nate <nwoods@hep.wisc.edu>
2017-02-06 10:05:53 +0100

    Install virtualenv stuff if needed, otherwise just activate it, as done in UWVV. Roll back to Rootpy v0.8.3 due to Graph methods broken in v0.9.0.

--- recipe/setupPython.sh
+++ recipe/setupPython.sh
@@ -1,8 +1,17 @@
-###     
-###     Set up a virtual environment with the newest version of rootpy
-###     
-###     Author: Nate Woods, U. Wisconsin
-###     
+#!/bin/bash
+####
+####    Set up a virtual environment with Rootpy, IPython, and Jupyter.
+####    Installs things the first time it runs, just activates the virtualenv
+####    thereafter.
+####
+####    Rootpy: Pythonic wrappers for PyROOT (using v0.8.3 because 0.9.0 is broken somehow)
+####    IPython: Nicer interactive Python
+####    Jupyter: IPython notebooks
+####    BRIL Work Suite: tools for luminosity (etc.)
+####
+####    Author: Nate Woods, U. Wisconsin
+####
+
 
 if [ "$zzt" == "" ]; then
     echo "Please set up this package with setup.sh before running this script."
@@ -11,11 +20,21 @@ fi
 
 export vpython="$zzt"/recipe/vpython
 
-# python "$zzt"/recipe/virtualenv/virtualenv.py "$vpython"
-# 
-# source "$vpython"/bin/activate
-# 
-# pip install -U pip
-# pip install -U rootpy
-pip install -U ipython
-pip install -U jupyter
+if [ ! -d "$vpython" ]; then
+    python "$zzt"/recipe/virtualenv/virtualenv.py "$vpython"
+
+    source "$vpython"/bin/activate
+
+    pip install -U pip
+    pip install -U rootpy==0.8.3
+    pip install -U ipython
+    pip install -U jupyter
+    if [ "$CMSSW_BASE" != "" ]; then
+        pip install brilws
+    fi
+else
+    source "$vpython"/bin/activate
+fi
+
+export PYTHONPATH="$vpython"/lib/python2.7/site-packages/:"$PYTHONPATH"
+
